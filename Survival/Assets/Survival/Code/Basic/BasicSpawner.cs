d43d5cc1e342e2365f58c25a3cec18e19de91ab4
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-11-29 23:24:20 +0200

    Reimplement basic spawner

--- Survival/Assets/Survival/Code/Basic/BasicSpawner.cs
+++ Survival/Assets/Survival/Code/Basic/BasicSpawner.cs
@@ -1,18 +1,82 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using Fusion;
+using Fusion.Sockets;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class BasicSpawner : MonoBehaviour
+namespace TMG.Survival.Gameplay
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
     {
-        
-    }
+        private NetworkRunner _runner;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            StartGame(GameMode.AutoHostOrClient);
+        }
+
+        private void OnGUI()
+        {
+            if (_runner == null)
+            {
+                if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
+                {
+                    StartGame(GameMode.Host);
+                }
+
+                if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
+                {
+                    StartGame(GameMode.Client);
+                }
+            }
+        }
+
+        public async void StartGame(GameMode mode)
+        {
+            _runner = gameObject.AddComponent<NetworkRunner>();
+            _runner.ProvideInput = true;
+
+            await _runner.StartGame(new StartGameArgs()
+            {
+                GameMode = mode,
+                SessionName = "TestRoom",
+                Scene = SceneManager.GetActiveScene().buildIndex,
+                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            });
+        }
+
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
+
+        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+
+        public void OnInput(NetworkRunner runner, NetworkInput input) { }
+
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+
+        public void OnConnectedToServer(NetworkRunner runner) { }
+
+        public void OnDisconnectedFromServer(NetworkRunner runner) { }
+
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request,
+            byte[] token) { }
+
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+
+        public void OnSceneLoadDone(NetworkRunner runner) { }
+
+        public void OnSceneLoadStart(NetworkRunner runner) { }
     }
 }
